72bea4a815ed0127f9a5f781a5c6cc704f11649d
Maxwell Machado <34950484+maxwellwolf@users.noreply.github.com>
2019-05-16 14:07:42 -0300

    Primeiro commit

---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Meu primeiro programa em java do curos formação java web!");
+		System.out.println("Java é orientado a objetos");
+
+	}
+
+}
