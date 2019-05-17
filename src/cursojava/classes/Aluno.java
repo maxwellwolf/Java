08e73d86a1548c76f78ebbb3f07a384f921220aa
Maxwell Machado <34950484+maxwellwolf@users.noreply.github.com>
2019-05-17 16:56:23 -0300

    Criando classes

---
+++ src/cursojava/classes/Aluno.java
@@ -0,0 +1,115 @@
+package cursojava.classes;
+
+public class Aluno {
+
+	String nome;
+	int    idade;
+	String dataNasc;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+	public Aluno() {
+
+
+	}
+	
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+
+	}
+	
+	public Aluno(String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+	
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	
+}
--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,13 +1,24 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+
 public class PrimeiraClasseJava {
-	
+
 	public static void main(String args[]) {
 		
+		Aluno aluno1 = new Aluno(); /*João*/
+		Aluno aluno2 = new Aluno(); //Pedro
 		
 		
-		System.out.println("Meu primeiro programa em java do curos formação java web!");
-		System.out.println("Java é orientado a objetos");
+		aluno1.setNome("Maxwell");
+		aluno1.setIdade(34);
+		aluno1.setDataNasc("11/02/1985");
+		aluno1.setNomeMae("Lina");
+		
+		
+		Aluno aluno3 = new Aluno("Maxwell", 34);
+
+		System.out.println("O seu nome é "+ aluno1.getNome() + " e a sua idade é " + aluno1.getIdade());
 
 	}
 
